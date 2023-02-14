cbbd87b60005aaaf83a429aa6a6a6999ecb9a033
MollyKressler <kressler.molly@gmail.com>
2023-02-14 17:57:21 +0000

    Starting boosted regression tree analysis
    Tried gbm.auto package. Its fine but maybe not the most appropriate for my use. Reverting to methods outlined in the rspatial BRT tutorial and Elith et al 2008.

--- teleost_glmms_chp3.R
+++ teleost_glmms_chp3.R
@@ -19,11 +19,11 @@ setwd('/Users/mollykressler/Documents/data_phd/resource_chp3')
 pacman::p_load(tidyverse,sf,ggplot2,gridExtra,flextable,vegan,sf,ggsn,lme4,ggeffects,effects,beepr,modelsummary,performance)
 
 # SHAPEFILE: bruvs data and habitat data (using sep22 updated habitat SOSF) joined together, with dist2shore calculated
-	joined<-st_as_sf(st_read('bruvs_data_joinedWITHhabitat_feb23.shp'))%>%rename(spp_abundance=spp_bnd,spp_richness=spp_rch,SW_Species=SW_Spcs,SW_Families=SW_Fmls,prop_brs=prp_brs,prop_ldsg=prp_lds,prop_medsg=prp_mds,prop_hdsg=prp_hds,prop_sarg=prp_srg,prop_urb_r=prp_rb_,prop_deep=prop_dp,dist2shore=dst2shr,Sphyraenidae=Sphyrnd,Scaridae=Scarida,Haemulidae=Haemuld,Gerreidae=Gerreid,Belonidae=Belonid,Sparidae=Sparida)%>%select(BRUV,jcode,spp_abundance,spp_richness,SW_Species,SW_Families,Sphyraenidae,Scaridae,Haemulidae,Gerreidae,Belonidae,Sparidae,prop_brs,prop_ldsg,prop_medsg,prop_hdsg,prop_sarg,prop_urb_r,prop_deep,dist2shore)
+	joined<-st_as_sf(st_read('bruvs_data_joinedWITHhabitat_feb23.shp'))%>%rename(spp_abundance=spp_bnd,spp_richness=spp_rch,SW_Species=SW_Spcs,SW_Families=SW_Fmls,prop_brs=prp_brs,prop_ldsg=prp_lds,prop_medsg=prp_mds,prop_hdsg=prp_hds,prop_sarg=prp_srg,prop_urb_r=prp_rb_,prop_deep=prop_dp,dist2shore=dst2shr,Sphyraenidae=Sphyrnd,Scaridae=Scarida,Haemulidae=Haemuld,Gerreidae=Gerreid,Belonidae=Belonid,Sparidae=Sparida)%>%dplyr::select(BRUV,jcode,spp_abundance,spp_richness,SW_Species,SW_Families,Sphyraenidae,Scaridae,Haemulidae,Gerreidae,Belonidae,Sparidae,prop_brs,prop_ldsg,prop_medsg,prop_hdsg,prop_sarg,prop_urb_r,prop_deep,dist2shore)
 
 # DATE FRAME: bruvs data and habitat data (using sep22 updated habitat SOSF) joined together, with dist2shore calculated. Use in models. 
 
-	joined_df<-read.csv('bruvs_DATAFRAME_joinedWITHhabitat_feb23.csv')%>%select(-X)
+	joined_df<-read.csv('bruvs_DATAFRAME_joinedWITHhabitat_feb23.csv')%>%dplyr::select(-X)
 	joined_df$BRUV<-as.factor(joined_df$BRUV)
 	joined_df$Season<-as.factor(joined_df$Season)
 	joined_df$SW_Species<-as.numeric(joined_df$SW_Species)
@@ -36,7 +36,9 @@ pacman::p_load(tidyverse,sf,ggplot2,gridExtra,flextable,vegan,sf,ggsn,lme4,ggeff
 	setwd('/Users/mollykressler/Documents/data_phd/resource_chp3')
 	land<-st_as_sf(st_read('bim_onlyland_noDots.kml'),crs='WGS84')
 
-	hab.noland<-st_difference(hab.grid,st_union(land)) # cut land out of hab.grid. 
+	hab.noland<-st_as_sf(st_difference(hab.grid,st_union(land))) # cut land out of hab.grid. 
+	hab.nloand2<-st_cast(hab.noland,'MULTIPOLYGON')
+	hab.atcentroids<-hab.nloand2%>%add_column(Longitude=st_coordinates(st_centroid(.))[,1],.before='prop_brs')%>%add_column(Latitude=st_coordinates(st_centroid(.))[,2],.before='Longitude')
 
 ############ ############ ############ ############ ############ 
 ############ ############ ############ ############ ############ 
@@ -47,26 +49,26 @@ pacman::p_load(tidyverse,sf,ggplot2,gridExtra,flextable,vegan,sf,ggsn,lme4,ggeff
 	## poisson with log link for Gerreidae
 	## for SWs? gamma? with negative inverse link. 
 
-sapply(joined_df,class)
-summary(joined_df)
+	sapply(joined_df,class)
+	summary(joined_df)
 
-# Shannon Index for Species
-Hspp<-glm(SW_Species~Season+dist2shore+prop_brs+prop_medsg+prop_sarg+prop_urb_r+prop_deep,data=joined_df,family=gaussian(link='identity'));beep(2)
-summary(Hspp)
-# Shannon Index for Families
-Hfam<-glm(SW_Families~Season+dist2shore+prop_brs+prop_medsg+prop_sarg+prop_urb_r+prop_deep,data=joined_df,family=gaussian(link='identity'));beep(2)
-summary(Hfam)
+	# Shannon Index for Species
+	Hspp<-glm(SW_Species~Season+dist2shore+prop_brs+prop_medsg+prop_sarg+prop_urb_r+prop_deep,data=joined_df,family=gaussian(link='identity'));beep(2)
+	summary(Hspp)
+	# Shannon Index for Families
+	Hfam<-glm(SW_Families~Season+dist2shore+prop_brs+prop_medsg+prop_sarg+prop_urb_r+prop_deep,data=joined_df,family=gaussian(link='identity'));beep(2)
+	summary(Hfam)
 
-# Gerreidae
-gerr<-glm(Gerreidae~Season+dist2shore+prop_brs+prop_medsg+prop_sarg+prop_urb_r+prop_deep,data=joined_df,family=poisson(link='log'));beep(2)
-summary(gerr)
+	# Gerreidae
+	gerr<-glm(Gerreidae~Season+dist2shore+prop_brs+prop_medsg+prop_sarg+prop_urb_r+prop_deep,data=joined_df,family=poisson(link='log'));beep(2)
+	summary(gerr)
 
 
-# save model RDS 
+	# save model RDS 
 
-saveRDS(gerr,'model_outputs/glm_gerreidae_feb23.RDS')
-saveRDS(Hspp,'model_outputs/glm_shannonindex_species_feb23.RDS')
-saveRDS(Hfam,'model_outputs/glm_shannonindex_families_feb23.RDS')
+	saveRDS(gerr,'model_outputs/glm_gerreidae_feb23.RDS')
+	saveRDS(Hspp,'model_outputs/glm_shannonindex_species_feb23.RDS')
+	saveRDS(Hfam,'model_outputs/glm_shannonindex_families_feb23.RDS')
 
 
 
@@ -74,12 +76,30 @@ saveRDS(Hfam,'model_outputs/glm_shannonindex_families_feb23.RDS')
 ############ ############ ############ ############ ############ 
 
 #### 13 & 14 February 2023 - BRTs example for gbm.auto package. I need to be able to run the models and export the surfaces (spatially formatted predictions). I'll test the package functionality usign the dataset they provide. 
+# install.packages("devtools")
 
+pacman::p_load(gbm,gbm.auto,tidyverse,sf,ggplot2,dismo)
 
+## try first with grids=NULL. will need to predict to maps outside of package...unsure how.
+## try with Gerreidae
+# not using gbm.auto. I tried and we didn't get along. 
 
+	#sw1<-gbm.auto(grids=NULL,samples=samples,expvar=c(2,13:20),resvar=c('SW_Species'),tc=c(9),lr=c(0.001,0.001),bf=0.9,fam1='bernoulli',fam2='gaussian',savedir='/Users/mollykressler/Documents/data_phd/resource_chp3',savegbm=TRUE,varint=TRUE)
 
+	#sw2<-gbm.loop(loops=10,samples=samples,expvar=c(2,13:20),resvar=c('SW_Species'),tc=c(9),lr=c(0.001,0.001),bf=0.9,fam1='bernoulli',fam2='gaussian',savedir='/Users/mollykressler/Documents/data_phd/resource_chp3',calcpreds=TRUE,savegbm=TRUE,multiplot=TRUE,)
 
+expl<-joined_df[c(2,13:20)]
+resp<-joined_df[c('Gerreidae')]
 
+sw.fam.bf<-gbm.bfcheck(samples,resvar=c('SW_Families')) #  binary bag fraction must be at least 0.087. n = 242 "Gaussian bag fraction must be at least 0.284. n = 74"
+sw.spp.bf<-gbm.bfcheck(samples,resvar=c('SW_Species')) # binary bag fraction must be at least 0.087. n = 242". "Gaussian bag fraction must be at least 0.091. n = 231"
+gerr.bf<-gbm.bfcheck(samples,resvar=c('Gerreidae')) ## 0.009, extremely low. should be able to model without issues related to bf. 
+
+sw1<-gbm.step(joined_df,gbm.x=c('Season','prop_brs','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_urb_r','prop_deep','dist2shore'),gbm.y=c('SW_Species'),tree.complexity=9,learning.rate=0.001,bag.fraction=0.75,family='gaussian',plot.main = TRUE) # 950 trees
+sw_species1<-sw1
+names(sw1)
+summary(sw1) # plots, and tables, the influence of each variable. 
+saveRDS(sw_species1,'SW_Species_brt_tc9_lr001_gaussian.RDS')
 
 
 
