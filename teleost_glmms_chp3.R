01bde4c23a501047267c9fc7ab710f6a77ab52f1
MollyKressler <kressler.molly@gmail.com>
2023-02-15 19:06:25 +0000

    Working through BRT tutorial with SW_Species
    Following the Appendix tutorial from rspatial, written by Jan Elith and John Leathwick.

--- teleost_glmms_chp3.R
+++ teleost_glmms_chp3.R
@@ -71,40 +71,61 @@ pacman::p_load(tidyverse,sf,ggplot2,gridExtra,flextable,vegan,sf,ggsn,lme4,ggeff
 	saveRDS(Hfam,'model_outputs/glm_shannonindex_families_feb23.RDS')
 
 
-
+############ ############ ############ ############ ############ 
+############ ############ ############ ############ ############ 
+			## Boosted Regression Tree Analysis ##
+	## for Gerreidae family, SW Species, and SW Families ##
 ############ ############ ############ ############ ############ 
 ############ ############ ############ ############ ############ 
 
-#### 13 & 14 February 2023 - BRTs example for gbm.auto package. I need to be able to run the models and export the surfaces (spatially formatted predictions). I'll test the package functionality usign the dataset they provide. 
-# install.packages("devtools")
+## using this tutorial [https://rspatial.org/raster/sdm/9_sdm_brt.html#predicting-to-new-data], which is in associaton with the Elith et al. 2008 paper. 
+	# not using gbm.auto. I tried and we didn't get along. 
 
-pacman::p_load(gbm,gbm.auto,tidyverse,sf,ggplot2,dismo)
+		#sw1<-gbm.auto(grids=NULL,samples=samples,expvar=c( ,13:20),resvar=c('SW_Species'),tc=c(9),lr=c(0.001,0.001),bf=0.9,fam1='bernoulli',fam2='gaussian',savedir='/Users/mollykressler/Documents/data_phd/resource_chp3',savegbm=TRUE,varint=TRUE)
 
-## try first with grids=NULL. will need to predict to maps outside of package...unsure how.
-## try with Gerreidae
-# not using gbm.auto. I tried and we didn't get along. 
+		#sw2<-gbm.loop(loops=10,samples=samples,expvar=c(2,13:20),resvar=c('SW_Species'),tc=c(9),lr=c(0.001,0.001),bf=0.9,fam1='bernoulli',fam2='gaussian',savedir='/Users/mollykressler/Documents/data_phd/resource_chp3',calcpreds=TRUE,savegbm=TRUE,multiplot=TRUE,)
 
-	#sw1<-gbm.auto(grids=NULL,samples=samples,expvar=c(2,13:20),resvar=c('SW_Species'),tc=c(9),lr=c(0.001,0.001),bf=0.9,fam1='bernoulli',fam2='gaussian',savedir='/Users/mollykressler/Documents/data_phd/resource_chp3',savegbm=TRUE,varint=TRUE)
+## going to work through the tutorial top to bottom for SW Speces to see if I can get what I want: diversity index predictions into the hab.noland grid.
 
-	#sw2<-gbm.loop(loops=10,samples=samples,expvar=c(2,13:20),resvar=c('SW_Species'),tc=c(9),lr=c(0.001,0.001),bf=0.9,fam1='bernoulli',fam2='gaussian',savedir='/Users/mollykressler/Documents/data_phd/resource_chp3',calcpreds=TRUE,savegbm=TRUE,multiplot=TRUE,)
+pacman::p_load(gbm,tidyverse,sf,ggplot2,dismo)
 
-expl<-joined_df[c(2,13:20)]
-resp<-joined_df[c('Gerreidae')]
-
+## using gbm.bfcheck to rule out bag fractions 
 sw.fam.bf<-gbm.bfcheck(samples,resvar=c('SW_Families')) #  binary bag fraction must be at least 0.087. n = 242 "Gaussian bag fraction must be at least 0.284. n = 74"
 sw.spp.bf<-gbm.bfcheck(samples,resvar=c('SW_Species')) # binary bag fraction must be at least 0.087. n = 242". "Gaussian bag fraction must be at least 0.091. n = 231"
 gerr.bf<-gbm.bfcheck(samples,resvar=c('Gerreidae')) ## 0.009, extremely low. should be able to model without issues related to bf. 
 
-sw1<-gbm.step(joined_df,gbm.x=c('Season','prop_brs','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_urb_r','prop_deep','dist2shore'),gbm.y=c('SW_Species'),tree.complexity=9,learning.rate=0.001,bag.fraction=0.75,family='gaussian',plot.main = TRUE) # 950 trees
-sw_species1<-sw1
-names(sw1)
-summary(sw1) # plots, and tables, the influence of each variable. 
-saveRDS(sw_species1,'SW_Species_brt_tc9_lr001_gaussian.RDS')
+## use gbm.step() to both cross validate the number of trees to use, and then run that model. 
+
+	sw1<-gbm.step(joined_df,gbm.x=c('Season','prop_brs','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_urb_r','prop_deep','dist2shore'),gbm.y=c('SW_Species'),tree.complexity=9,learning.rate=0.001,bag.fraction=0.75,family='gaussian',plot.main = TRUE) # 950 trees
+	sw_species1<-sw1
+	names(sw1)
+	summary(sw1) # plots, and tables, the influence of each variable. 
+	#saveRDS(sw_species1,'model_outputs/SW_Species_brt_tc9_lr001_gaussian.RDS')
+
+## okay let's say we're happy with that model. And we're going to skip simplifying the model. Because we want all the explanatory vars to stay in =>
+## Time to plot the functions and fitted values from the model 
+
+gbm.plot(sw_species1, plot.layout=c(3,3),write.title=FALSE) 
+	# the fitted functions from the BRT model
+	# these can be misleading based on the distirbution of observations (can give the wrong indication of what different values of predictor do to the response if the data is sparse at certain predictor values)
+	# the next plot function, gbm.plot.fits, can help investgate if this is the case.
+
+gbm.plot.fits(sw_species1)
+	# this plots the fitted values irt their predictor
+	# in this data set (BRUVS, SW_Species) you can see that there are a lot of zeros, but otherwise many variables have a good spread of fitted values across the range for each predictor. 
+	# above each graph is a 'wtm' value. This is the 'weighted mean' of the fitted values in relation to each non-factor predictor. 
+
+## Interactions - investgate if pairwise interactiosn exits in the data as modelled by the BRT. And then you can plot them. 
+
+find.int.swspp<-gbm.interactions(sw_species1) # run the function
+find.int.swspp$interactions # print the interacton matrix
+find.int.swspp$rank.list # prints just the list of non-zero interactions 
+
+gbm.perspec(sw_species1,9,4) # plot pairwise interactions one at a time (or rather 2 covariates at a time with the response). Numbers correspond to the variables (easy way to identify them is the rank.list call from above). This also provides the maxmum fitted value by this interaction
+gbm.perspec(sw_species1,9,6) 
 
 
-
-
-
+## Predicting to new data. 
 
 
 
