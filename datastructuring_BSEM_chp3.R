fb51611257a376dbdca0f5e309e13df40e0416b8
MollyKressler <kressler.molly@gmail.com>
2023-05-17 16:12:33 +0100

    Starting path analysis

--- datastructuring_BSEM_chp3.R
+++ datastructuring_BSEM_chp3.R
@@ -6,7 +6,7 @@
 
 ## Load workspace
 
-pacman::p_load(tidyverse,sf,ggplot2,cowplot,patchwork,flextable,sf,ggsn)
+pacman::p_load(tidyverse,sf,ggplot2,cowplot,patchwork,flextable,sf,ggsn,dismo,gbm)
 setwd('/Users/mollykressler/Documents/data_phd')
 
 ## Load data
@@ -20,7 +20,7 @@ sharksANDhabitat<-st_as_sf(st_read('cleaned_detections/detections20192020_NOghos
 buffs<-st_as_sf(st_read('buffers_2019_2020.shp'),crs='WGS84')%>%mutate(buff_ar=st_area(geometry))
 
 	# check:
-	ggplot()+geom_sf(data=sharksANDhabitat)+geom_sf(data=buffs,alpha=0.5,fill='violetred2',col=NA)+theme_bw()
+	ggplot()+geom_sf(data=sharksANDhabitat)+geom_sf(data=buffs[33,],alpha=0.5,fill='violetred2',col=NA)+theme_bw()
 	# SHOULD SEE: 35 black dots (receivers) and pale pink crcles around each black dot (the effective detection range of 211m)
 
 ### Modelling dataframe 
@@ -82,52 +82,96 @@ buffs<-st_as_sf(st_read('buffers_2019_2020.shp'),crs='WGS84')%>%mutate(buff_ar=s
 
 		land<-st_as_sf(st_read('bim_onlyland_noDots.kml'),crs='WGS84')
 
+	### There's an issue with the buffers (r33 specifically) where it wasn't identifying the habitat in the buffer. Here's code to fix it in the document. 
 		buffswithhab<-st_join(buffs,sharksANDhabitat)
+		r33<-buffs%>%filter(buffID=='r33')
+		ggplot()+geom_sf(data=r33withhab2)+geom_sf(data=land)
 
-		sf4preds<-buffswithhab%>%group_by(buffID)%>%filter(row_number()==1)%>%dplyr::select('buffID','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_urb_r','prop_deep','prop_marsh','prop_man','prop_spong','prop_inlvg','prop_unkwn','dist_cmg')%>%mutate(dist2shore=st_distance(st_centroid(geometry),st_union(land)))
+		sf_use_s2(FALSE) # turn speherical geometry off
+
+		buffs2.join<-st_join(st_make_valid(r33),st_make_valid(hab.new))%>%mutate(hab_geo=st_geometry(st_intersection(st_make_valid(st_geometry(hab.new)),st_make_valid(st_geometry(r33)))))%>%mutate(hab_area=st_area(hab_geo))
+
+		buffs2.spread<-buffs2.join%>%spread(key= 'habitat',value = 'hab_area',fill=0)%>%group_by(buffID)%>%summarise(across(c('buff_ar','highdensg','lowdensg','meddensg','sargassum','rocks.urb',),max))%>%mutate(prop_ldsg=lowdensg/buff_ar,prop_medsg=meddensg/buff_ar,prop_hdsg=highdensg/buff_ar,prop_sarg=sargassum/buff_ar,prop_urb_r=rocks.urb/buff_ar)%>%dplyr::select(buffID,buff_ar,prop_ldsg,prop_medsg,prop_hdsg,prop_sarg,prop_urb_r,geometry)%>%add_column(prop_unkwn=0,.before='geometry')%>%
+			add_column(prop_marsh=0,prop_deep=0,prop_inlvg=0,prop_brs=0,prop_man=0,prop_spong=0)
+			
+		buffs.new<-buffs2.spread
+			{
+			buffs.new$prop_brs<-as.numeric(buffs.new$prop_brs)
+			buffs.new$prop_ldsg<-as.numeric(buffs.new$prop_ldsg)
+			buffs.new$prop_medsg<-as.numeric(buffs.new$prop_medsg)
+			buffs.new$prop_hdsg<-as.numeric(buffs.new$prop_hdsg)
+			buffs.new$prop_man<-as.numeric(buffs.new$prop_man)
+			buffs.new$prop_sarg<-as.numeric(buffs.new$prop_sarg)
+			buffs.new$prop_marsh<-as.numeric(buffs.new$prop_marsh)
+			buffs.new$prop_urb_r<-as.numeric(buffs.new$prop_urb_r)
+			buffs.new$prop_deep<-as.numeric(buffs.new$prop_deep)
+			buffs.new$prop_inlvg<-as.numeric(buffs.new$prop_inlvg)
+			buffs.new$prop_unkwn<-as.numeric(buffs.new$prop_unkwn)
+			}
+		
+		cmg<-st_as_sf(st_read('habitat_model/centroid_mangroves_north.shp'))
+
+		buffs.new2<-buffs.new%>%add_column(dist_cmg=st_distance(st_centroid(buffs.new$geometry),cmg),.before='geometry')
+
+		r33withhab2<-buffs.new2%>%
+			dplyr::select('buffID','prop_brs','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_urb_r','prop_deep','prop_marsh','prop_man','prop_spong','prop_inlvg','prop_unkwn','dist_cmg')%>%
+			mutate(dist2shore=st_distance(st_centroid(geometry),st_union(land)))%>%
+			filter(row_number()==1)
+		
+			r33withhab2$dist2shore<-as.numeric(r33withhab2$dist2shore)
+			r33withhab2$dist_cmg<-as.numeric(r33withhab2$dist_cmg)
+		
+			r33df<-as.data.frame(r33withhab2)%>%dplyr::select(-geometry)
+
+
+		sf4preds<-buffswithhab%>%group_by(buffID)%>%filter(row_number()==1)%>%dplyr::select('buffID','prop_brs','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_urb_r','prop_deep','prop_marsh','prop_man','prop_spong','prop_inlvg','prop_unkwn','dist_cmg')%>%mutate(dist2shore=st_distance(st_centroid(geometry),st_union(land)))%>%filter(buffID!='r33')
 			sf4preds$dist2shore<-as.numeric(sf4preds$dist2shore)
-		df4preds<-as.data.frame(sf4preds)%>%select(-geometry)
+			sf4preds$dist_cmg<-as.numeric(sf4preds$dist_cmg)
+		sf4preds<-bind_rows(sf4preds,r33withhab2)
+		stopifnot(nrow(sf4preds)==35)
+		df4preds<-as.data.frame(sf4preds)%>%dplyr::select(-geometry)
 		df4preds$dist2shore<-as.numeric(df4preds$dist2shore)
-		### you left off here, you need to predict the brts into the buffer areas
-		pacman::p_load(dismo,gbm)
 
-		sf4<-left_join(df4preds,sf4preds,by='buffID',keep=FALSE)%>%
-			dplyr::select(buffID,geometry)
+		sf4<-sf4preds
+
+		df4preds$buffID<-as.factor(df4preds$buffID)
+		sf4$buffID<-as.factor(sf4$buffID)
+
 
 		for(i in simple.models){
 			mod<-get(i)
 			predicts<-predict.gbm(mod,df4preds,n.trees=mod$gbm.call$best.trees,type='response')
 			predicts<-as.data.frame(predicts)
 			predicts.df<-bind_cols(df4preds,predicts)
-			sf4b<-sf4%>%mutate(preds=predicts.df$predicts)
+			sf4b<-left_join(sf4,predicts.df)
 			new=paste0(i,'PD')
-			sf4<-sf4b%>%dplyr::rename_with(.fn=~paste0(new),.cols='preds')
+			sf4<-sf4b%>%dplyr::rename_with(.fn=~paste0(new),.cols='predicts')
 			}
 
 			# back transform the gerreidae (poisson, log)
-			sf4<-sf4%>%mutate(bt.gerr.simpPD=exp(.$simple_gerrPD),.before='geometry')%>%rename(sppsimpPD=simple_sppPD)
+			sf4<-sf4%>%mutate(bt.gerr.simpPD=exp(simple_gerrPD),.before='geometry')%>%rename(sppsimpPD=simple_sppPD)
 
+			sf44<-st_make_valid(sf4)
 			# save the updated sf4preds and df4preds
-				st_write(sf4,'sf_with_predictions_INTO_BUFFERS_fromBRTs_feb23.shp',append=FALSE) # append command set to FALSE replaces the old file with the new one. If set to TRUE it would add the new data as further layers.
+				st_write(sf44,'sf_with_predictions_INTO_BUFFERS_fromBRTs_feb23.shp',append=FALSE) # append command set to FALSE replaces the old file with the new one. If set to TRUE it would add the new data as further layers.
 				st_write(sf4,'sf_with_predictions_INTO_BUFFERS_fromBRTs_feb23.csv',delete_layer=TRUE,delete_dsn=TRUE)
 
 
 
-
-
-
-
-
-
-
-fishiness_in_buffers<-st_as_sf(st_read('sf_with_predictions_INTO_BUFFERS_fromBRTs_feb23.shp'),crs='WGS84')%>%dplyr::select(-smpl_PD)
-
-# spatially join sharks & habitat to fishiness in buffers 
-
-data<-st_join(sharksANDhabitat,fishiness_in_buffers)
+fishiness_in_buffers<-st_as_sf(st_read('sf_with_predictions_INTO_BUFFERS_fromBRTs_feb23.shp'),crs='WGS84')%>%dplyr::select(-smpl_PD)%>%rename('prop_brs'='prp_brs','prop_ldsg'='prp_lds','prop_medsg'='prp_mds','prop_hdsg'='prp_hds','prop_sarg'='prp_srg','prop_marsh'='prp_mrs','prop_urb_r'='prp_rb_','prop_deep'='prop_dp','prop_spong'='prp_spn','prop_unkwn'='prp_nkw','dist2shore'='dst2shr')
+f<-fishiness_in_buffers%>%filter(buffID=='r33')
 # yay!
 
-	st_write(data,'resource_chp3/data_for_bayes_structural_EQ_modelling_optionC_sharkiness_fishiness_habitat_may23.shp',driver='ESRI Shapefile')
-	st_write(data,'resource_chp3/data_for_bayes_structural_EQ_modelling_optionC_sharkiness_fishiness_habitat_may23.csv',driver='CSV')
+# spatially join sharks & habitat to fishiness in buffers 
+not.needed.vars<-c("difftim","sgmnt_s","sgmnt_n","SgmntID","ghost" , "FID","station","nearest_mg",'prop_brs','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_marsh','prop_urb_r','prop_deep','prop_spong','prop_unkwn','prop_inlvg','prop_man','dist_cmg')
+sharksANDhabitat2<-sharksANDhabitat%>%
+	dplyr::select(-all_of(not.needed.vars))
+
+data5<-st_join(sharksANDhabitat2,fishiness_in_buffers)
+nrow(sharksANDhabitat2)
+summary(data5)
+
+	st_write(data5,'resource_chp3/data_for_bayes_structural_EQ_modelling_optionC_sharkiness_fishiness_habitat_may23.shp',driver='ESRI Shapefile',append=FALSE)
+	st_write(data5,'resource_chp3/data_for_bayes_structural_EQ_modelling_optionC_sharkiness_fishiness_habitat_may23.csv',driver='CSV',delete_layer=TRUE,delete_dsn=TRUE)
 
 
--- pathanalysisBSEM_chp3.R
+++ pathanalysisBSEM_chp3.R
@@ -1,124 +1,440 @@
-## Path analysis for directionality of fish abundance, shark presence, and habitat composition, on each other.
+## Path analysis for fish abundance, shark presence, and habitat composition, on each other.
 
 ## Modelling code from paper Bennett, S., Harris, M. P., Wanless, S., Green, J. A., Newell, M. A., Searle, K. R., & Daunt, F. (2022). Earlier and more frequent occupation of breeding sites during the non‐breeding season increases breeding success in a colonial seabird. Ecology and Evolution, 12(9). https://doi.org/10.1002/ece3.9213.
 
 ## created 10 May 2023 by Molly M Kressler 
 
+
+#########################################################################
+######################### RUN AT OPEN ###################################
+
 ## Load Workspace 
 
 pacman::p_load(tidyverse,sf,nimble)
 
-setwd('/Users/mollykressler/Documents/data_phd/resource_chp3')
+#setwd('/Users/mollykressler/Documents/data_phd/resource_chp3')
 
-not.needed.vars<-c("difftim","sgmnt_s","sgmnt_n","SgmntID","ghost" , "FID","station")
-data<-read.csv('data_for_bayes_structural_EQ_modelling_optionC_sharkiness_fishiness_habitat_may23.csv')%>%dplyr::select(-not.needed.vars)
-data$PIT<-as.factor(data$PIT)
+data<-read.csv('data_for_bayes_structural_EQ_modelling_optionC_sharkiness_fishiness_habitat_may23.csv')#%>%rename('prop_brs'='prp_','prop_ldsg'='prp_lds','prop_medsg'='prp_mds','prop_hdsg'='prp_hds','prop_sarg'='prp_srg','prop_marsh'='prp_mrs','prop_urb_r'='prp_rb_','prop_deep'='prop_dp','prop_spong'='prp_spn','prop_unkwn'='prp_nkw')
+data$PIT<-as.character(data$PIT)
+data$buffID<-as.character(data$buffID)
 	
 stopifnot(nrow(data)==33701) # check 
-#############################################
-###### Write model in BUGS with NIMBLE ######
 
-# parts of nimble model: data, params, distirbutiions, logical operators/process models, derived params/generated quantities (optional)
+################################
+###### Define Sharkiness  ######
+## counts of detections per individual per Site (buffer/receiver)
 
-### variables 
-# sharkiness = detections (points), presence.
-# fishiness = either Gerreidae abundance or Shannon Index of Species, as predicted by simplified BRTs.
-# habitat = 12 proportions of habitat types.
-# individual = PIT, factor in data
+habitats<-c('prop_brs','prop_ldsg','prop_medsg','prop_hdsg','prop_sarg','prop_marsh','prop_urb_r','prop_deep','prop_unkwn')
+fishmetrics<-c('bt_g_PD','sppsmPD')
 
-########### MODEL 1: shark point detectons, Gerreidae abundance predictions (BRT simplified model), habitat (12 variables, proportions of habitat)
+fishANDhab<-data%>%
+	dplyr::select(buffID,dist2shore,dst_cmg,unlist(habitats),unlist(fishmetrics))%>%
+	group_by(buffID)%>%
+	filter(row_number()==1)
 
-model1<-nimblecode({})
+data2<-data%>%
+	dplyr::select(PIT,buffID)%>%
+	group_by(PIT,buffID)%>%
+	tally()
+data3<-expand.grid(PIT=unique(data2$PIT),buffID=unique(data2$buffID))%>%
+	left_join(.,data2)%>%
+	mutate(n=replace_na(n,0))%>%
+	left_join(.,fishANDhab,by='buffID')%>%
+	mutate(gerries=bt_g_PD)%>%
+	mutate(fishy=sppsmPD*gerries)%>% 
+	mutate(standard.shark=((n-mean(n))/sd(n)))%>% 
+	mutate(standard.fish=((fishy-mean(fishy))/sd(fishy)))%>%
+	mutate(standard.lds=((prop_ldsg-mean(prop_ldsg))/sd(prop_ldsg)))%>% 
+	mutate(standard.mds=((prop_medsg-mean(prop_medsg))/sd(prop_medsg)))%>% 
+	mutate(standard.unkn=((prop_unkwn-mean(prop_unkwn))/sd(prop_unkwn)))%>% 
+	mutate(standard.dist2shore=((dist2shore-mean(dist2shore))/sd(dist2shore)))%>% 
+	mutate(standard.distcmg=((dst_cmg-mean(dst_cmg))/sd(dst_cmg)))%>%
+	dplyr::select(PIT,buffID,standard.shark,standard.fish,standard.dist2shore,standard.distcmg,standard.lds,standard.mds)
+
+data3$PIT<-as.factor(data3$PIT)
+data3$buffID<-as.factor(data3$buffID)
+
+stopifnot(!is.na(data3$standard.lds) & !is.na(data3$prop_brs) & nrow(data3)==560)
+
+summary(data3)
+head(data3,5)
+
+
+######################### END ###################################
+#################################################################
+
+
+#####################################################################
+######################### CHECKS  ###################################
+
+
+###
+## Compare Fishy (cross metric) and Diversity and Gerreidae
+
+	ggplot(data=data3)+geom_histogram(aes(x=gerries),fill='violetred2',alpha=0.5, binwidth=5)+geom_histogram(aes(x=fishy),fill='cadetblue3',alpha=0.5, binwidth=5)+theme_bw()+xlab('Value')+ylab('Count')+theme(legend.position='topright')
+
+	ggplot(data=data3)+geom_histogram(aes(x=standard.fish),fill='goldenrod3',alpha=0.5, binwidth=.5)+theme_bw()
+
+###
+## DISTRIBUTION CHECKS 
+	## for standardised fish cross metric variable 'fishy' (count of gerries x shannon index for species) 
+	summary(data3$standard.fish) # min=-0.83, max = 3.395
+
+		ggplot(data=data3)+geom_histogram(aes(x=(standard.fish)),fill='cadetblue4',alpha=0.8,binwidth=0.5)+theme_bw()
+		#normal I guess
+
+
+	## for standardised shark variable 'n' (sum(detections) per individual @ each receiver)
+
+		ggplot(data=data3)+geom_histogram(aes(x=standard.shark),fill='cadetblue4',alpha=0.5,binwidth=.5)+theme_bw()
+
+######################### END ###################################
+#################################################################
+
+
+########################################################
+################# BUGS models with NIMBLE ##############
+
+
+##########################################
+########### MODEL 1A, RECEIVER LEVEL MODEL: MEAN CENTRED AND STANDARDISED DATA - shark point detectons (counts per receiver for each individual, sample size N=35), Gerreidae abundance predictions (BRT simplified model), habitat 
+	## for habitat variables, we used the types identified in previous modelling as influencing habitat selection. For sharks, this is distance to the central mangrove point and medium density seagrass; for Gerreidae, this is distance to shore and low density seagrass; and for the diversity index for species, we included only the habitats with relative influence in the BRTs > 15%, which are distance to the shore and low density seagrass. 
+	## random effect for SITE but not for shark individual
+
+# all vars as normal, because they are + and - and centred on 0. 
+
+# arguments to pass later to nimble in nimbleModel()
+
+	myCovs<-c('n','buffID','fishy','dist2shore','dist_cmg','prop_ldsg','prop_medsg')
+	myConstants<-list(N=560,numGroups=35)
+	myData<-list(y=data3,x=myCovs)
+	myInits<-list(alpha = ,beta = ,theta = )
+
+	nimbleModel(constants=myConstants,data=myData, inits=list()) ## need to define myCovs
+
+##########################
+
+modelCode1a<-nimbleCode({
 
 ##########################
 ######### priors #########
 
-#### prior for sharkiness ####
-for(i in 1:3){
-	a[i] ~ dnorm(0,.0001)
-}
+#### prior for sharkiness #### 
+for(i in 1:5){
+	a[i] ~ dnorm(0,.001) }
 # prior for intercept - sharks 
-	b ~ dnorm(0,.0001)
+	b ~ dnorm(0,.001)
+# prior for residual variance - sharks 
+	tau.shark ~ dgamma(0.001,0.001) # 
+	sigma.shark <- 1/tau.shark 
 
-## random effect for individual on sharks 
-# prior for precision of random effect (individual), then calculated to variance 
-for(i in 1:individuals){
-	epsi_shark[i] ~ dnorm(0,tau.epsi_shark)
-}
+# group-level effect of buffID on sharkiness. prior for variance (epsi_site) calculated from inverse of precision (epsi_site)
+for(i in 1:N){
+	epsi_shark[i]~ dnorm(0,tau.epsi_shark)}
 tau.epsi_shark ~ dgamma(.001,.001)
 sigma.epsi_shark ~ 1/tau.epsi_shark
 
-# prior for residual variance - sharkiness 
-tau.shark ~ dgamma(.001,.001)
-sigma.shark <- 1/tau.shark
 
-
-#### prior for fishiness ####
-for(i in 1:3){
-	c[i] ~ dnorm(0,.0001)
-}
+#### prior for fishiness (Gerreidae) ####
+for(i in 1:5){
+	c[i] ~ dnorm(0,0.001) }
 # prior for intercept - fishiness
-	d ~ dnorm(0,.0001)
+	d ~ dnorm(0,0.001)
 
-# random effect for site ID on fishiness 
+# prior for residual variance (precision) - fishiness
+tau.fish ~ dgamma(0.001,0.001) 
+sigma.fish ~ 1/tau.fish 
+
+# group-level effect for site ID on fishiness (epsi_fish)
 # prior for precision of random effect (buffID), then calculated to variance 
-for(i in 1:sites){
-	epsi_fish[i] ~ dnorm(0,tau.epsi_fish)
-}
-tau.epsi_fish ~ dgamma(.001,.001)
+for(i in 1:N){ #n.obsv? or should it related to the grouping
+	epsi_fish[i] ~ dnorm(0,tau.epsi_fish)}
+tau.epsi_fish ~ dgamma(0.001,0.001)
 sigma.epsi_fish ~ 1/tau.epsi_fish
 
-# prior for residual variance - fishiness
-tau.fish ~ dgamma(.001,.001)
-sigma.fish ~ 1/tau.fish
+
+#### prior for habitats: distance to shore, distance to refuge, low density seagrass, medium density seagrass ####
+ # not even sure I need to specify these since they aren't on the LHS
+# priors - habitat: dist2shore
+	e ~ dnorm(0.001,0.001) # slope
+	ee ~ dnorm(0.001,0.001) # intercept
+
+# priors - habitat: dist_cmg
+	f ~ dnorm(0.001,0.001) # slope
+	ff ~ dnorm(0.001,0.001) # intercept
+
+# priors - habitat: prop_ldsg
+	g ~ dnorm(0.001,0.001) # slope
+	gg ~ dnorm(0.001,0.001) # intercept
+
+# priors - habitat: prop_medsg
+	h ~ dnorm(0.001,0.001) # slope
+	hh ~ dnorm(0.001,0.001) # intercept
 
 
-#### prior for habitat ####
 
-# prior for intercept - habitat
-e ~ dnorm(0,0.0001)
+#########################################################
+######### Likelihoods - data and process models #########	
 
-# prior for slope - habitat 
-f ~ dnorm(0,0.0001)
+### data model for sharkiness 
+	for(i in 1:N){
+		shark[i] ~ dnorm(shark.mu[i],tau.shark) }
 
-# random effect of buffID on habitat values. prior for variance (sigma) calculated from inverse of precision (tau)
-for(i in 1:sites){
-	epsi.site[i]~dnorm(0,tau.epsi_site)
-}
-tau.epsi_site ~ dgamma(.001,.001)
-sigma.epsi_site ~ 1/tau.epsi_site
+# process model for sharkiness ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg ? + epsi_site?
+	for(i in 1:N){
+		shark.mu[i] ~ b + a[1]*standard.fish[i] + a[2]*standard.dist2shore[i] + a[3]*standard.distcmg[i] + a[4]*standard.lds[i] + a[5]*standard.mds[i] + epsi_shark[buffID[i]]}
 
-# prior for residual variance - site of hab sample
-tau.site ~ dgamma(.001,.001)
-sigma.site ~ 1/tau.site
-
-
-###########################################
-######### data and process models #########	
-
-### data model for sharkiness ~ hab + fish + indiv
-
-# process model for sharkiness ~ hab + fish + indiv
-
-
-### data model for fishiness ~ shark+ hab + indiv 
-
-# process model for fishiness ~ shark + hab + indiv
+### data model for fishiness 
+	for(i in 1:N){
+		fish[i] ~ dnorm(fish.mu[i],tau.fish) }
+# process model for fishiness ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg  + epsi_fish
+	for(i in 1:N){
+		fish.mu[i] ~ d + c[1]*standard.shark[i] + c[2]*standard.dist2shore[i] + c[3]*standard.distcmg[i] + c[4]*standard.lds[i] + c[5]*standard.mds[i] + epsi_fish[buffID[i]]}
 
 
 ######### derived parameters #########
 # for estmating total pathways 
 # write one for each route through the pathway diagram
 
-path[1]<- # shark ~ hab + indiv
-path[2]<- # fish ~ hab 
-path[3]<- # shark ~ fish + indiv
-path[4]<- # fish ~ shark
-path[5]<- # shark ~ fish + indiv + hab
-path[6]<- # fish ~ shark + hab 
+	path[1]<- a[2]*a[3]*a[4]*a[5] 
+		# shark ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+	path[2]<- c[2]*c[3]*c[4]*c[5] 
+		# fish ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+	path[3]<- a[1] 
+		# shark ~ fish 
+	path[4]<- c[1] 
+		# fish ~ shark 
+	path[5]<- a[1]*c[2]*c[3]*c[4]*c[5] 
+		# shark ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg # with coeff for habitat as they went into fish and came through
+	path[6]<- c[1]*a[2]*a[3]*a[4]*a[5]
+		# fish ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+
+
+}) # end of model code 
+
+		# alternate derived paths 
+			path[1]<- e*f*g*h*a[2]*a[3]*a[4]*a[5] 
+				# shark ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+			path[2]<- e*f*g*h*c[2]*c[3]*c[4]*c[5] 
+				# fish ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+			path[3]<- a[1] 
+				# shark ~ fish 
+			path[4]<- c[1] 
+				# fish ~ shark 
+			path[5]<- e*f*g*h*a[1]*c[2]*c[3]*c[4]*c[5] 
+				# shark ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg # with coeff for habitat as they went into fish and came through
+			path[6]<- e*f*g*h*c[1]*a[2]*a[3]*a[4]*a[5]
+				# fish ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+
+
+##########################
+## Compile the model code
+##########################
+
+
+	myCovs<-c('standard.fish','standard.shark','standard.dist2shore','standard.distcmg','standard.lds','standard.mds','buffID')
+	myConstants<-list(N=560,buffID=35) # if above, for group-level effects, the 'N' should be related to the numbe of groups, then the 'N' should be changed to numGroups, or numGroups changed to buffID. 
+	myData<-list(
+		# tell nimble the covariates 
+		standard.fish = data3$standard.fish,
+		standard.shark = data3$standard.shark,
+		standard.dist2shore = data3$standard.dist2shore,
+		standard.distcmg = data3$standard.distcmg,
+		standard.lds = data3$standard.lds,
+		standard.mds = data3$standard.mds,
+		# tell nimble the constants
+		N=560,
+		buffID=as.numeric(factor(data3$buffID))
+		)
+	initial.values <- function() list(theta = runif(1,0,1))
+	init.values<-list(alpha=1,beta=1,theta= rep(0.1,myConstants$N))
+
+model1a<-nimbleModel(code=modelCode1a, name="model1a",data=myData)
+	
+	model1a$getNodeNames
+
+model1aMCMC.output<-nimbleMCMC(code=modelCode1a,data=myData,inits=initial.values,niter=5000,nburnin=1000,nchains=1)
+
+##########################
+## Compile the MCMC
+##########################
+##########################
+## Run the MCMC
+##########################
+##########################
+## Evaluate the samples from the mcmc 
+##########################
 
 
 
 
+
+######################### END ###################################
+#################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################
+##########################################
+##########################################
+########### This is before I remembered I need to mean centre and standardse the variables
+
+	##MODEL 1B, RECEIVER LEVEL MODEL: shark point detectons (counts per receiver for each individual, sample size N=35), Gerreidae abundance predictions (BRT simplified model), habitat 
+		## for habitat variables, we used the types identified in previous modelling as influencing habitat selection. For sharks, this is distance to the central mangrove point and medium density seagrass; for Gerreidae, this is distance to shore and low density seagrass; and for the diversity index for species, we included only the habitats with relative influence in the BRTs > 15%, which are distance to the shore and low density seagrass. 
+		## random effect for SITE but not for shark individual
+
+		## 15/5: fishy variable with gamma distribution; 'n' for sharks with a neg-binomial distribution
+
+	# arguments to pass later to nimble in nimbleModel()
+
+		myCovs<-c('n','buffID','fishy','dist2shore','dist_cmg','prop_ldsg','prop_medsg')
+		myConstants<-list(n.obsv=560,numGroups=35)
+		myData<-list(y=data3,x=myCovs)
+		myInits<-list(alpha = ,beta = ,theta = )
+
+		nimbleModel(constants=myConstants,data=myData, inits=list()) ## need to define myCovs
+
+	model1<-nimblecode({})
+
+	##########################
+	######### priors #########
+
+	#### prior for sharkiness #### 
+	for(i in 1:3){
+		a[i] ~ dnorm(0,.001) # trying negative binomial for sharks 'n', dnegbin(prob = p, size = r), a mean and a dispersion
+	}
+	# prior for intercept - sharks 
+		b ~ dnorm(0,.001)
+	# prior for residual variance - sharks 
+		tau.shark ~ exp(1) # 
+		sigma.shark <- 1/tau.shark # dispersion 
+
+
+	#### prior for fishiness (Gerreidae) ####
+	for(i in 1:3){
+		c[i] ~ dgamma(0.01,0.01) 
+	}
+	# prior for intercept - fishiness
+		d ~ dgamma(0.01,0.01)
+
+	# prior for residual variance (precision) - fishiness
+	tau.fish ~ dinvgamma(.01,.01) # inverse gamma? 
+	sigma.fish ~ 1/tau.fish^2 #(shape/rate^2)
+
+	# group-level effect for site ID on fishiness (epsi_fish)
+	# prior for precision of random effect (buffID), then calculated to variance 
+	for(i in 1:n.obsv){
+		epsi_fish[i] ~ dnorm(0,tau.epsi_fish)
+	}
+	tau.epsi_fish ~ dgamma(.001,.001)
+	sigma.epsi_fish ~ 1/tau.epsi_fish
+
+
+	#### prior for habitats: distance to shore, distance to refuge, low density seagrass, medium density seagrass ####
+
+	# priors - habitat: dist2shore
+		e ~ dbeta(0,.1) # slope
+		ee ~ dbeta(0,.1) # intercept
+
+	# priors - habitat: dist_cmg
+		f ~ dbeta(0,.1) # slope
+		ff ~ dbeta(0,.1) # intercept
+
+	# priors - habitat: prop_ldsg
+		g ~ dbeta(0,.1) # slope
+		gg ~ dbeta(0,.1) # intercept
+
+	# priors - habitat: prop_medsg
+		h ~ dbeta(0,.1) # slope
+		hh ~ dbeta(0,.1) # intercept
+
+
+	# group-level effect of buffID on habitat values. prior for variance (epsi_site) calculated from inverse of precision (epsi_site)
+	for(i in 1:n.obsv){
+		epsi_site[i]~dnorm(0,tau.epsi_site)
+	}
+	tau.epsi_site ~ dgamma(.001,.001)
+	sigma.epsi_site ~ 1/tau.epsi_site
+
+	# prior for residual variance - site of hab sample
+	tau.site ~ dgamma(.001,.001)
+	sigma.site ~ 1/tau.site
+
+
+	###########################################
+	######### data and process models #########	
+
+	### data model for sharkiness 
+		for(i in 1:n.obsv){
+			shark[y] ~ dnbinom(shark.mu[i],sigma.shark) # should this be tau.shark or sigma.shark? tau is the dispersion or the precision? blogs say the dispersion
+			# dnbinom, needs a mean (mu) and a dispersion (sigma?tau?) 
+			# lost in the sauce
+		}
+
+	# process model for sharkiness ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg ? + epsi_site?
+		for(i in 1:n.obsv){
+			shark.mu[y] ~ b + a[1]*gerries[y] + a[2]dist2shore[y] + a[3]dist_cmg[y] + a[4]prop_ldsg[y] + a[5]prop_medsg[y] + epsi_site[buffID[y]] # ?? also epsi_fish? no, only in the one that specifies fish (fish on the LHS). 
+		}
+
+	### data model for fishiness 
+		for(i in 1:n.obsv){
+			fish[y] ~ dgamma(fish.shape[i],tau.fish) # tau.fish is actually the rate (?..i think) currently, its got a gamma distribution, and is used to calculate the precision 
+		}
+	# process model for fishiness ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg  + epsi_fish
+		for(i in 1:n.obsv){
+			fish.shape[y] ~ d + c[1]*n[y] + c[2]dist2shore[y] + c[3]dist_cmg[y] + c[4]prop_ldsg[y] + c[5]prop_medsg[y] + epsi_fish[buffID[y]]
+		}
+
+
+	######### derived parameters #########
+	# for estmating total pathways 
+	# write one for each route through the pathway diagram
+
+		habs: dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+	 
+	### you left off here on Friday (12/5), about to re-work this section so it is correct and updated with the multiple hab types and their coefficients. 
+
+	path[1]<-  # shark ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+	path[2]<-  # fish ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg + epsi_site
+	path[3]<-  # shark ~ fish 
+	path[4]<-  # fish ~ shark + epsi_site
+	path[5]<- # shark ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg 
+	path[6]<- # fish ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg + epsi_site
+
+	# ^^ some should have ntercepts? when? why?
+
+
+
+
+
+
+
+
+
+
+	######################################################
+	#### Code for model with individual random effect ####
+
+	## random effect for individual on sharks 
+	# prior for precision of random effect (individual), then calculated to variance 
+		for(i in 1:PIT){
+			epsi_shark[i] ~ dnorm(0,tau.epsi_shark)
+		}
+		tau.epsi_shark ~ dgamma(.001,.001)
+		sigma.epsi_shark ~ 1/tau.epsi_shark
+
+		# prior for residual variance - sharkiness 
+		tau.shark ~ dgamma(.001,.001)
+		sigma.shark <- 1/tau.shark
 
 
