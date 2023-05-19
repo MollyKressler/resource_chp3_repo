f06aa518e6a7dbfea70231324c86aba9bdf55db4
MollyKressler <kressler.molly@gmail.com>
2023-05-19 15:49:30 +0100

    Updates to model code and plots

--- pathanalysisBSEM_chp3.R
+++ pathanalysisBSEM_chp3.R
@@ -10,9 +10,9 @@
 
 ## Load Workspace 
 
-pacman::p_load(tidyverse,sf,nimble)
+pacman::p_load(tidyverse,sf,nimble,MCMCvis,flextable,webshot2)
 
-#setwd('/Users/mollykressler/Documents/data_phd/resource_chp3')
+setwd('/Users/mollykressler/Documents/data_phd/resource_chp3')
 
 data<-read.csv('data_for_bayes_structural_EQ_modelling_optionC_sharkiness_fishiness_habitat_may23.csv')#%>%rename('prop_brs'='prp_','prop_ldsg'='prp_lds','prop_medsg'='prp_mds','prop_hdsg'='prp_hds','prop_sarg'='prp_srg','prop_marsh'='prp_mrs','prop_urb_r'='prp_rb_','prop_deep'='prop_dp','prop_spong'='prp_spn','prop_unkwn'='prp_nkw')
 data$PIT<-as.character(data$PIT)
@@ -49,10 +49,10 @@ data3<-expand.grid(PIT=unique(data2$PIT),buffID=unique(data2$buffID))%>%
 	mutate(standard.unkn=((prop_unkwn-mean(prop_unkwn))/sd(prop_unkwn)))%>% 
 	mutate(standard.dist2shore=((dist2shore-mean(dist2shore))/sd(dist2shore)))%>% 
 	mutate(standard.distcmg=((dst_cmg-mean(dst_cmg))/sd(dst_cmg)))%>%
-	dplyr::select(PIT,buffID,standard.shark,standard.fish,standard.dist2shore,standard.distcmg,standard.lds,standard.mds)
+	mutate(buffIDnum=str_remove(data3$buffID,"r"))%>%
+	dplyr::select(PIT,buffIDnum,standard.shark,standard.fish,standard.dist2shore,standard.distcmg,standard.lds,standard.mds)
 
-data3$PIT<-as.factor(data3$PIT)
-data3$buffID<-as.factor(data3$buffID)
+data3$buffIDnum<-as.numeric(data3$buffIDnum)
 
 stopifnot(!is.na(data3$standard.lds) & !is.na(data3$prop_brs) & nrow(data3)==560)
 
@@ -103,14 +103,6 @@ head(data3,5)
 
 # all vars as normal, because they are + and - and centred on 0. 
 
-# arguments to pass later to nimble in nimbleModel()
-
-	myCovs<-c('n','buffID','fishy','dist2shore','dist_cmg','prop_ldsg','prop_medsg')
-	myConstants<-list(N=560,numGroups=35)
-	myData<-list(y=data3,x=myCovs)
-	myInits<-list(alpha = ,beta = ,theta = )
-
-	nimbleModel(constants=myConstants,data=myData, inits=list()) ## need to define myCovs
 
 ##########################
 
@@ -121,92 +113,70 @@ modelCode1a<-nimbleCode({
   
   #### prior for sharkiness #### 
   for(i in 1:5){
-    a[i] ~ dnorm(0,.001) }
+    a[i] ~ dnorm(0,.001) 
+}
   # prior for intercept - sharks 
   b ~ dnorm(0,.001)
   # prior for residual variance - sharks 
   tau.shark ~ dgamma(0.001,0.001) # 
-  sigma.shark <- (1/tau.shark)
+  sigma.shark <- sqrt(1/tau.shark)
   
   # group-level effect of buffID on sharkiness. prior for variance (epsi_site) calculated from inverse of precision (epsi_site)
-  for(i in 1:N){
-    epsi_shark[i]~ dnorm(0,tau.epsi_shark)}
+  for(i in 1:B){
+    epsi_shark[i]~ dnorm(0,tau.epsi_shark)
+}
   tau.epsi_shark ~ dgamma(.001,.001)
-  sigma.epsi_shark <- (1/tau.epsi_shark)
+  sigma.epsi_shark <- sqrt(1/tau.epsi_shark)
   
   
   #### prior for fishiness (Gerreidae) ####
   for(i in 1:5){
-    c[i] ~ dnorm(0,0.001) }
+    c[i] ~ dnorm(0,0.001) 
+}
   # prior for intercept - fishiness
   d ~ dnorm(0,0.001)
   
   # prior for residual variance (precision) - fishiness
   tau.fish ~ dgamma(0.001,0.001) 
-  sigma.fish <- (1/tau.fish)
+  sigma.fish <- sqrt(1/tau.fish) # gives sd 
   
   # group-level effect for site ID on fishiness (epsi_fish)
   # prior for precision of random effect (buffID), then calculated to variance 
-  for(i in 1:N){ #n.obsv? or should it related to the grouping
-    epsi_fish[i] ~ dnorm(0,tau.epsi_fish)}
+  for(i in 1:B){ # B is no. of buffers
+    epsi_fish[i] ~ dnorm(0,tau.epsi_fish)
+}
   tau.epsi_fish ~ dgamma(0.001,0.001)
-  sigma.epsi_fish <- (1/tau.epsi_fish)
-  
-  
-  #### prior for habitats: distance to shore, distance to refuge, low density seagrass, medium density seagrass ####
-  # not even sure I need to specify these since they aren't on the LHS
-  # priors - habitat: dist2shore
-  e ~ dnorm(0.001,0.001) # slope
-  ee ~ dnorm(0.001,0.001) # intercept
-  
-  # priors - habitat: dist_cmg
-  f ~ dnorm(0.001,0.001) # slope
-  ff ~ dnorm(0.001,0.001) # intercept
-  
-  # priors - habitat: prop_ldsg
-  g ~ dnorm(0.001,0.001) # slope
-  gg ~ dnorm(0.001,0.001) # intercept
-  
-  # priors - habitat: prop_medsg
-  h ~ dnorm(0.001,0.001) # slope
-  hh ~ dnorm(0.001,0.001) # intercept
-  
-  
+  sigma.epsi_fish <- sqrt(1/tau.epsi_fish)
   
   #########################################################
   ######### Likelihoods - data and process models #########	
   
   ### data model for sharkiness 
   for(i in 1:N){
-    shark[i] ~ dnorm(shark.mu[i],tau.shark) }
-  
+    standard.shark[i] ~ dnorm(shark.mu[i],tau.shark)   
   # process model for sharkiness ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg ? + epsi_site?
-  for(i in 1:N){
-    shark.mu[i] <- b + a[1]*standard.fish[i] + a[2]*standard.dist2shore[i] + a[3]*standard.distcmg[i] + a[4]*standard.lds[i] + a[5]*standard.mds[i] + epsi_shark[buffID[i]]}
-  
+    shark.mu[i] <- b + epsi_shark[buffID[i]] #+ a[1]*standard.fish.pred[i] + a[2]*standard.dist2shore[i] + a[3]*standard.distcmg[i] + a[4]*standard.lds[i] + a[5]*standard.mds[i] 
   ### data model for fishiness 
-  for(i in 1:N){
-    fish[i] ~ dnorm(fish.mu[i],tau.fish) }
+    standard.fish[i] ~ dnorm(fish.mu[i],tau.fish) 
   # process model for fishiness ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg  + epsi_fish
-  for(i in 1:N){
-    fish.mu[i] <- d + c[1]*standard.shark[i] + c[2]*standard.dist2shore[i] + c[3]*standard.distcmg[i] + c[4]*standard.lds[i] + c[5]*standard.mds[i] + epsi_fish[buffID[i]]}
-  
-  
+    fish.mu[i] <- d + epsi_fish[buffID[i]]#+ c[1]*standard.shark.pred[i] + c[2]*standard.dist2shore[i] + c[3]*standard.distcmg[i] + c[4]*standard.lds[i] + c[5]*standard.mds[i] 
+}
+
   ######### derived parameters #########
   # for estmating total pathways 
   # write one for each route through the pathway diagram
   
-  path[1]<- a[2]*a[3]*a[4]*a[5] 
+  path[1]<- b*a[2]*a[3]*a[4]*a[5] 
   # shark ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg 
-  path[2]<- c[2]*c[3]*c[4]*c[5] 
+  path[2]<- d*c[2]*c[3]*c[4]*c[5] 
   # fish ~ dist2shore + dist_cmg + prop_ldsg + prop_medsg 
-  path[3]<- a[1] 
+  path[3]<- b*a[1] 
   # shark ~ fish 
-  path[4]<- c[1] 
+  path[4]<- d*c[1] 
   # fish ~ shark 
-  path[5]<- a[1]*c[2]*c[3]*c[4]*c[5] 
+  path[5]<- b*a[1]*c[2]*c[3]*c[4]*c[5] 
   # shark ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg # with coeff for habitat as they went into fish and came through
-  path[6]<- c[1]*a[2]*a[3]*a[4]*a[5]
+  path[6]<- d*c[1]*a[2]*a[3]*a[4]*a[5]
   # fish ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg 
   
   
@@ -216,9 +186,8 @@ modelCode1a<-nimbleCode({
 ## Compile the model code
 ##########################
 
-
 myCovs<-c('standard.fish','standard.shark','standard.dist2shore','standard.distcmg','standard.lds','standard.mds','buffID')
-myConstants<-list(N=560,buffID=as.numeric(factor(data3$buffID))) # ,buffID=35# if above, for group-level effects, the 'N' should be related to the numbe of groups, then the 'N' should be changed to numGroups, or numGroups changed to buffID. 
+myConstants<-list(N=560,B=max(data3$buffIDnum),buffID=data3$buffIDnum) 
 myData<-list(
   # tell nimble the covariates 
   standard.fish = data3$standard.fish,
@@ -226,28 +195,150 @@ myData<-list(
   standard.dist2shore = data3$standard.dist2shore,
   standard.distcmg = data3$standard.distcmg,
   standard.lds = data3$standard.lds,
-  standard.mds = data3$standard.mds
+  standard.mds = data3$standard.mds,
+  standard.shark.pred = data3$standard.shark,
+  standard.fish.pred = data3$standard.fish
   # tell nimble the constants
   #N=560,
   #buffID=as.numeric(factor(data3$buffID))
 )
-initial.values <- function() list(theta = runif(1,0,1))
-init.values<-list(alpha=1,beta=1,theta= rep(0.1,myConstants$N))
 
-model1a<-nimbleModel(code=modelCode1a, name="model1a",data=myData,constants = myConstants)
+init.values<-list(a=rnorm(5,0,1),b=rnorm(1),c=rnorm(5,0,1),d=rnorm(1),tau.shark=rgamma(1,0.001,0.001),tau.fish=rgamma(1,0.001,0.001))
+	
+model1a<-nimbleModel(code=modelCode1a, name="model1a",data=myData,constants = myConstants,inits=init.values) #define the model
+Cm1a<-compileNimble(model1a) # compile the model
 
-model1a$getNodeNames
+#saveRDS(Cm1a,'nimblemodelCompiled_model1a_BSEM_resourceChp3.RDS')
+#	Cm1a<-readRDS('nimblemodelCompiled_model1a_BSEM_resourceChp3.RDS')
 
-model1aMCMC.output<-nimbleMCMC(code=modelCode1a,data=myData,inits=initial.values,niter=5000,nburnin=1000,nchains=1)
 ##########################
-## Compile the MCMC
+## Compile the MCMC, Run and Evaluate samples
 ##########################
+
+C1a.MCMC.output<-nimbleMCMC(code=modelCode1a,data=myData,constants= myConstants, inits=init.values,niter=5000,nburnin=1000,nchains=4) #,summary=TRUE,samples=TRUE
+
+samples_chain1of4_C1aMCMC<-as.data.frame(C1a.MCMC.output$chain1)%>%mutate(chain='1')%>%mutate(iter=seq(1,4000,1))
+samples_chain2of4_C1aMCMC<-as.data.frame(C1a.MCMC.output$chain2)%>%mutate(chain='2')%>%mutate(iter=seq(1,4000,1))
+samples_chain3of4_C1aMCMC<-as.data.frame(C1a.MCMC.output$chain3)%>%mutate(chain='3')%>%mutate(iter=seq(1,4000,1))
+samples_chain4of4_C1aMCMC<-as.data.frame(C1a.MCMC.output$chain4)%>%mutate(chain='4')%>%mutate(iter=seq(1,4000,1))
+
+samples_fourchains_C1aMCMC<-bind_rows(samples_chain1of4_C1aMCMC,samples_chain2of4_C1aMCMC,samples_chain3of4_C1aMCMC,samples_chain4of4_C1aMCMC)%>%
+	rename(a1='a[1]',a2='a[2]',a3='a[3]',a4='a[4]',a5='a[5]',c1='c[1]',c2='c[2]',c3='c[3]',c4='c[4]',c5='c[5]')
+
+head(samples_fourchains_C1aMCMC)
+
+
+## evaluate chain sampling
+
+paramnames<-colnames(samples_fourchains_C1aMCMC%>%group_by(chain))
+paramnames<-paramnames[1:24]
+plot_list = list()
+for(i in paramnames){
+	var<-sym(i)
+	p = ggplot(data=samples_fourchains_C1aMCMC%>%group_by(chain),aes(x=iter,col=chain,y=!!var))+
+	geom_line()+
+	ylab(NULL)+
+	ggtitle(paste(var))+
+	theme_bw()
+	#print(p) # include if you want it to print each one.
+	plot_list[[i]] = p
+	filename = paste('fourchainsplot_model1a_nimbleBSEM_', var, '.png',sep="")
+	png(filename, units='in', res=1100, width=7,height=3)
+	print(plot_list[[i]])
+	dev.off()
+}
+
 ##########################
-## Run the MCMC
+## Posterior Inference 
 ##########################
-##########################
-## Evaluate the samples from the mcmc 
-##########################
+
+# numeric summaries
+
+mcmc_summary_Cmodel1a<-MCMCsummary(C1a.MCMC.output,round=2)%>%
+		tibble::rownames_to_column()%>%
+		rename_with(str_to_title)%>%
+		flextable()%>%
+		theme_alafoli()%>%
+		set_header_labels(rowname = 'Coefficient',SD='Sd')%>%
+		align(align = 'center', part = 'all')%>%
+		font(fontname = 'Times', part = 'all')%>%
+		color(color='black',part='all')%>%
+		fontsize(size = 10, part = 'all')%>%
+		autofit()
+	save_as_image(mcmc_summary_Cmodel1a,'mcmc_summary_4chains_5000iter_1000burnin_model1a_nimbleBSEM.png',webshot='webshot')
+
+# caterpillar plots 
+
+
+MCMCplot(C1a.MCMC.output,ci=c(50,95)) # point = median, thick line = 50% CI, thin line = 95% CI 
+MCMCsummary(C1a.MCMC.output,round=2)%>%
+		tibble::rownames_to_column()%>%
+		rename_with(str_to_title)%>%
+		ggplot()+
+		geom_pointrange(aes(x=Rowname,ymin='2.5%',ymax='97.5%'))
+
+ggsave(,file='mcmc_medianANDeffectTails_4chains_5000iter_1000burnin_model1a_nimbleBSEM.png',device=png,dpi=800, units='in',height=5,width=3)
+
+# trace and density plots
+
+coeffNintercept<-colnames(samples_fourchains_C1aMCMC,do.NULL=TRUE,prefix='row')
+
+
+MCMCtrace(C1a.MCMC.output,pdf=TRUE,ind=TRUE, Rhat=TRUE, n.eff=TRUE) # ind = TRUE, separate density lines per chain. # pdf = FALSE, don't export to a pdf automatically. 
+
+
+# DAG 
+#Cm1a$plotGraph()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ? calculate the path estimates - take the estimates for each coeff for each chain, then multiply across the path
+
+a1<-c(C1a.MCMC.output$chain1[,'a[1]'],
+	C1a.MCMC.output$chain2[,'a[1]'],
+	C1a.MCMC.output$chain3[,'a[1]'],
+	C1a.MCMC.output$chain4[,'a[1]'])
+a2<-c(C1a.MCMC.output$chain1[,'a[2]'],
+	C1a.MCMC.output$chain2[,'a[2]'],
+	C1a.MCMC.output$chain3[,'a[2]'],
+	C1a.MCMC.output$chain4[,'a[2]'])
+a3<-c(C1a.MCMC.output$chain1[,'a[3]'],
+	C1a.MCMC.output$chain2[,'a[3]'],
+	C1a.MCMC.output$chain3[,'a[3]'],
+	C1a.MCMC.output$chain4[,'a[3]'])
+a4<-c(C1a.MCMC.output$chain1[,'a[4]'],
+	C1a.MCMC.output$chain2[,'a[4]'],
+	C1a.MCMC.output$chain3[,'a[4]'],
+	C1a.MCMC.output$chain4[,'a[4]'])
+b<-c(C1a.MCMC.output$chain1[,'b'],
+	C1a.MCMC.output$chain2[,'b'],
+	C1a.MCMC.output$chain3[,'b'],
+	C1a.MCMC.output$chain4[,'b'])
+path1<-a1*a2*a3*a4*b # gives a massive number..not right. missing something
+mean(path1)
+quantile(path1)
+path1%>%as_tibble()%>%ggplot()+geom_histogram(aes(x=value))+theme_bw()
+
+### 18/5 you left off here, you want to make a flextable of chains summary table rounded to 2 decimals places. you need to refresh on what the nEff is. additionally, just go through the whole Gimenez tutorial. this should help and be the figures/tables that rich is expecting. 
+
+
+
+
+
+
+
+
 
 
 
@@ -257,7 +348,21 @@ model1aMCMC.output<-nimbleMCMC(code=modelCode1a,data=myData,inits=initial.values
 #################################################################
 
 
-
+  ### data model for sharkiness 
+  for(i in 1:N){
+    standard.shark[i] ~ dnorm(shark.mu[i],tau.shark) }
+  
+  # process model for sharkiness ~ fish + dist2shore + dist_cmg + prop_ldsg + prop_medsg ? + epsi_site?
+  for(i in 1:N){
+    shark.mu[i] <- b + a[1]*standard.fish[i] + a[2]*standard.dist2shore[i] + a[3]*standard.distcmg[i] + a[4]*standard.lds[i] + a[5]*standard.mds[i] + epsi_shark[buffID[i]]}
+  
+  ### data model for fishiness 
+  for(i in 1:N){
+    standard.fish[i] ~ dnorm(fish.mu[i],tau.fish) }
+  # process model for fishiness ~ shark + dist2shore + dist_cmg + prop_ldsg + prop_medsg  + epsi_fish
+  for(i in 1:N){
+    fish.mu[i] <- d + c[1]*standard.shark[i] + c[2]*standard.dist2shore[i] + c[3]*standard.distcmg[i] + c[4]*standard.lds[i] + c[5]*standard.mds[i] + epsi_fish[buffID[i]]}
+  
 
 
 
